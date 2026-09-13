659ab1d86208c926eb5d9c4f7ebf3c61f0c27c4e
Ahmed’s SHI'SHIR <mim672562@gmail.com>
2026-09-13 20:14:52 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -3,7 +3,7 @@ const moment = require("moment-timezone");
 module.exports = {
   config: {
     name: "owner",
-    aliases: ["admininfo", "info", "ownerinfo"],
+    aliases: ["admininfo", "boss", "ownerinfo"],
     version: "3.0",
     author: "xalman",
     countDown: 5,
