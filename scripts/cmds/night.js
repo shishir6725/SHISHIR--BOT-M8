fbc8587e691afa64012e623c056d863ccb08b270
Ahmed’s SHI'SHIR <mim672562@gmail.com>
2026-09-14 18:01:52 +0600

    Create night.js

---
+++ scripts/cmds/night.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "night",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "night",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Good night")==0 || event.body.indexOf("good night")==0 || event.body.indexOf("Good night")==0 || event.body.indexOf("ghumabo")==0) {
+		var msg = {
+				body: "Good night 🌉✨ Bye tc 💫🥀 Sweet dreams 😴",
+				attachment: fs.createReadStream(__dirname + `/cache/night.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😴", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
