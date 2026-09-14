4e2228a0907e46e39c831fc629a06a9456fd539a
Ahmed’s SHI'SHIR <mim672562@gmail.com>
2026-09-14 08:55:15 +0600

    Update uptime5.js

--- scripts/cmds/uptime5.js
+++ scripts/cmds/uptime5.js
@@ -27,7 +27,7 @@ module.exports = {
 
       const ping = Date.now() - event.timestamp;
       const cpuUsage = os.loadavg()[0].toFixed(2);
-      const owner = "ꫝɴ֟፝ɪᴋ ɪsʟꫝᴍ ѕꫝᴅɪᴋ";
+      const owner = "AhmeD'z SHI'SHIR ";
 
       const canvas = Canvas.createCanvas(1000, 500);
       const ctx = canvas.getContext("2d");
