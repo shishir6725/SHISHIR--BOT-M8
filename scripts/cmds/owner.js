b452961c2307db707d9b4beb830fdfc195071103
Ahmed’s SHI'SHIR <mim672562@gmail.com>
2026-09-18 20:42:31 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "owner",
     version: "1.3.0",
-    author: "亗•𝘔𝘈𝘔𝘜𝘕✿᭄",
+    author: "Farhan-Khan",
     role: 0,
     shortDescription: "Owner information with image",
     category: "Information",
@@ -17,26 +17,67 @@ module.exports = {
 
   onStart: async function ({ api, event }) {
     const ownerText = 
-`╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : 亗•Ahmed'z SHISHIR ✿᭄
-│🧸 Nɪᴄᴋ       : YOUR ABBu
-│ 🎂 Aɢᴇ        : 17+
-│ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
-│ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 1ɴᴅ Yᴇᴀʀ
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : Sirajganj   
-├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : https://www.facebook.com/share/1Y7HF4jVB5/
-│ 💬 Messenger: m.me/61592841571046
-│ 📞 WhatsApp  : wa.me/01493--26
-╰────────────────╯`;
+`╔═══❖𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢❖═══╗
+ 
+ ‎⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆ 
+  [🤖]↓:𝐁𝐎𝐓→𓆩👑𓆪 𝑶𝑾𝑵𝑬𝑹:↓
+  ➤ 『 𝐀𝐡𝐦𝐞𝐃’𝐬 𝐒𝐇𝐈'𝐒𝐇𝐈𝐑 』
+ ‎⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+
+╠══❖『𝐁𝐈𝐎 𝐀𝐃𝐌𝐈𝐍』❖══╣
+ ⊱༅༎😽💚༅༎⊱
+
+꧁༒☬ 𝐒𝐇𝐈𝐒𝐇𝐈𝐑 𝐁𝐎𝐓 ☬༒꧂
+
+🖤 Your Group's New Digital Family 🤖
+
+⚡ Smart • Fast • Unique 🙂  
+
+  ⊱༅༎😽💚༅༎⊱
+╠═════════════════╣
+
+[🏠]↓:𝐀𝐃𝐃𝐑𝐄𝐒𝐒:↓
+➤ 『 𝐒𝐈𝐑𝐀𝐉𝐆𝐀𝐍𝐉 』
+‎
+⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+
+[🕋]↓:𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍:↓
+➤ 『 𝐈𝐒𝐋𝐀𝐌 』
+
+‎⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+
+[🚻]↓:𝐆𝐄𝐍𝐃𝐄𝐑:↓
+➤ 『 𝐌𝐀𝐋𝐄 』
+
+‎⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+
+[💞]↓:𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏:↓
+➤ 『 𝐒𝐈𝐍𝐆𝐋𝐄 』
+
+‎⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+
+[🧑‍🔧]↓:𝐖𝐎𝐑𝐊:↓
+➤ 『 𝐒𝐭𝐮𝐝𝐞𝐧𝐭 』
+
+‎⋆✦⋆═══🅲🅾🅽🆃🅰🅲🆃═══⋆✦⋆
+
+[📞] 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣
+➤ https://wa.me/+61592841571046
+
+[🌍] 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)
+➤ https://m.me/61592841571046
+
+[🌍] 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)
+➤ Vai 2nd account bolte kisui nai sob saspent hoiye jai🙂
+
+╚═══❖𝗧𝗛𝗔𝗡𝗞 𝗬𝗢𝗨❖═══╝`;
 
     const cacheDir = path.join(__dirname, "cache");
     const imgPath = path.join(cacheDir, "owner.jpg");
 
     if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
 
-    const imgLink = "https://i.imgur.com/K8Wj1bD.jpeg";
+    const imgLink = "https://i.imgur.com/T8IQ8lb.jpeg";
 
     const send = () => {
       api.sendMessage(
@@ -52,6 +93,6 @@ module.exports = {
 
     request(encodeURI(imgLink))
       .pipe(fs.createWriteStream(imgPath))
-      .on("close", send);
+      .on("close", send)
   }
 };
